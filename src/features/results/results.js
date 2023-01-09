cd9e6e9f4a4532f2476aaf9ee82ff2521494e4f9
nomadicagent <brendenmitchum@gmail.com>
2023-01-09 15:58:19 -0800

    connect search bar to results

--- src/features/results/results.js
+++ src/features/results/results.js
@@ -1,35 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-// import { data as result } from '../../api/api';
-
-export default function Results() {
-  const [children, setChildren] = useState([]);
-  
-
-  const fetchData = () => {
-    fetch("https://www.reddit.com/search.json?q=hockey")
-      .then(response => {
-        return response.json()
-      })
-      .then(data => {
-        setChildren(data.data.children)
-      })
-  }
-
-  useEffect(() => {
-    fetchData();
-    
-  }, [])
+export default function Results(props) {
 
   return (
-    <div className='results'>
-      {children.map((result) => (
-        <div className='result'>
-          <p>{result.data.title}</p>
-          <p>{result.data.num_comments}</p>
-          <img src={result.data.thumbnail} alt='' align='bottom'/>
-        </div>
-      ))}
+    <div className='main-page'>
+      
+      <div className='results'>
+        {props.children.map((result) => (
+          <div className='result'>
+            <p>{result.data.title}</p>
+            <p>{result.data.num_comments}</p>
+            <img src={result.data.thumbnail} alt='' align='bottom'/>
+          </div>
+          ))}
+      </div>
     </div>
     )
 }
