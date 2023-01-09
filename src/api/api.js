add340b958beaf87b53b4d92e5efd134da925fe5
nomadicagent <brendenmitchum@gmail.com>
2023-01-09 12:12:12 -0800

    edit results.js to render basic results page with title, number of comments & thumbnail of results

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,11 @@
 
-  export const fetchData = () => {
-    fetch("https://www.reddit.com/r/popular.json")
-      .then(response => {
-        return response.json()
-      })
-      .then(data => {
-        setUsers(data)
-      })
-  }
+  
+//   async function fetchData(url) {
+//     const response = await fetch(url);
+
+//     return response.json();
+//     }
+
+// export const data = await fetchData('https://www.reddit.com/r/popular.json');
+
+//try JSON.stringify(jsonData) or JSON.parse(jsonData)
--- src/features/results/results.js
+++ src/features/results/results.js
@@ -1,26 +1,35 @@
 import React, { useEffect, useState } from 'react';
 
+// import { data as result } from '../../api/api';
 
 export default function Results() {
-  const [users, setUsers] = useState([])
+  const [children, setChildren] = useState([]);
+  
 
   const fetchData = () => {
-    fetch("https://www.reddit.com/r/popular.json")
+    fetch("https://www.reddit.com/search.json?q=hockey")
       .then(response => {
         return response.json()
       })
       .then(data => {
-        setUsers(data)
+        setChildren(data.data.children)
       })
   }
 
   useEffect(() => {
-    fetchData()
+    fetchData();
+    
   }, [])
 
   return (
     <div className='results'>
-        <p>{users}</p>
+      {children.map((result) => (
+        <div className='result'>
+          <p>{result.data.title}</p>
+          <p>{result.data.num_comments}</p>
+          <img src={result.data.thumbnail} alt='' align='bottom'/>
+        </div>
+      ))}
     </div>
     )
 }
